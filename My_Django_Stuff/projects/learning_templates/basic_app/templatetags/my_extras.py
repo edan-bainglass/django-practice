36c300bec4ffb168b11df9bc61502702e438a33e
simplogic86 <edan.bainglass@gmail.com>
2022-01-07 09:02:13 -0600

    Added template filters to basic_app/index.html

---
+++ My_Django_Stuff/projects/learning_templates/basic_app/templatetags/my_extras.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def cut(value, arg):
+    """
+    This cuts out all values of "arg" from the string.
+    """
+    return value.replace(arg, '')
--- My_Django_Stuff/projects/learning_templates/basic_app/views.py
+++ My_Django_Stuff/projects/learning_templates/basic_app/views.py
@@ -5,7 +5,8 @@ from django.shortcuts import render
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, 'basic_app/index.html')
+    context_dict = {'text': 'hello world', 'number': 100}
+    return render(request, 'basic_app/index.html', context_dict)
 
 
 def other(request: HttpRequest) -> HttpResponse:
