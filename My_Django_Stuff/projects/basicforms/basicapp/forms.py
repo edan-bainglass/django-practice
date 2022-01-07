5fe7d27e12aa78c4ed8ea0b6a1db70f45f045b51
simplogic86 <edan.bainglass@gmail.com>
2022-01-07 09:02:13 -0600

    Added custom validation

--- My_Django_Stuff/projects/basicforms/basicapp/forms.py
+++ My_Django_Stuff/projects/basicforms/basicapp/forms.py
@@ -1,7 +1,23 @@
 from django import forms
+from django.core import validators
+
+
+# custom validator
+def check_for_z(value: str):
+    if value[0] != 'Z':
+        raise forms.ValidationError("Name must start with Z")
 
 
 class Form(forms.Form):
-    name = forms.CharField()
+    name = forms.CharField(validators=[check_for_z])
     email = forms.EmailField()
+    verify_email = forms.EmailField(label='Enter your email again:')
     text = forms.CharField(widget=forms.Textarea)
+
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data['email']
+        vmail = all_clean_data['verify_email']
+
+        if email != vmail:
+            raise forms.ValidationError("Email doesn't match!")
