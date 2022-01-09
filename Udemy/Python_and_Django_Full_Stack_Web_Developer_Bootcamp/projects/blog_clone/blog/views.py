764bb0982f58543b498055c7495cad744bda9be0
simplogic86 <edan.bainglass@gmail.com>
2022-01-09 11:52:33 -0600

    Completed blog clone part 5

--- Udemy/Python_and_Django_Full_Stack_Web_Developer_Bootcamp/projects/blog_clone/blog/views.py
+++ Udemy/Python_and_Django_Full_Stack_Web_Developer_Bootcamp/projects/blog_clone/blog/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
+
 from django.utils import timezone
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
-from django.contrib.auth.mixins import LoginRequiredMixin
+
 from django.views.generic import (TemplateView, ListView, DetailView,
                                   CreateView, UpdateView, DeleteView)
 
@@ -74,3 +80,57 @@ class DraftListView(LoginRequiredMixin, ListView):
         """docstring"""
         return Post.objects.filter(
             published_date__isnull=True).order_by('-published_date')
+
+
+###############################################################################
+
+
+@login_required
+def publish_post(request: HttpRequest, pk: int) -> HttpResponse:
+    """docstring"""
+
+    post: Post = get_object_or_404(Comment, pk=pk)
+    post.publish()
+
+    return redirect('post_detail', pk=pk)
+
+
+@login_required
+def add_comment_to_post(request: HttpRequest, pk: int) -> HttpResponse:
+    """docstring"""
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == 'POST':
+        form = CommentForm(request.Post)
+
+        if form.is_valid():
+            comment: Comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+
+    else:
+        form = CommentForm()
+
+    return render(request, 'blog/comment-form.html', {'form': form})
+
+
+@login_required
+def approve_comment(request: HttpRequest, pk: int) -> HttpResponse:
+    """docstring"""
+
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+@login_required
+def remove_comment(request: HttpRequest, pk: int) -> HttpResponse:
+    """docstring"""
+
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+
+    return redirect('post_detail', pk=post_pk)
